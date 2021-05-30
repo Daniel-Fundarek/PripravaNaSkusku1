93a7c63c908bb27df1ca61f19f73d3a855eec702
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2021-05-30 13:57:58 +0200

    dfd2

--- src/main/java/sk/stuba/fei/uim/oop/MyCanvas.java
+++ src/main/java/sk/stuba/fei/uim/oop/MyCanvas.java
@@ -26,12 +26,17 @@ public class MyCanvas extends Canvas implements MouseMotionListener, MouseListen
 
     @Override
     public void mousePressed(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        actualRect = new MyRectangle(x,y,1,1, Color.GREEN);
 
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        obj.add(actualRect);
+        repaint();
+        actualRect = null;
     }
 
     @Override
@@ -46,11 +51,47 @@ public class MyCanvas extends Canvas implements MouseMotionListener, MouseListen
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        if(actualRect !=null){
+            int dx = e.getX();
+            int dy = e.getY();
+            if (dx > this.x && dy < this.y){
+                actualRect.y = dy;
+                actualRect.height = this.y - dy;
+                actualRect.width =  dx - this.x;
+
+            }
+            else if(dx < this.x && dy < this.y){
+                actualRect.x = dx;
+                actualRect.y = dy;
+                actualRect.width = this.x - dx;
+                actualRect.height = this.y - dy;
+            }
+            else if(dx < this.x && dy > this.y) {
+                actualRect.x = dx;
+                actualRect.width = this.x - dx;
+                actualRect.height =  dy - this.y;
+            }
+            else {
+                actualRect.width = dx - this.x;
+                actualRect.height = dy - this.y;
+            }
+            repaint();
+        }
 
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
 
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        for (MyRectangle a: obj){
+            a.paintRect(g);
+        }
+        if(actualRect != null){
+            actualRect.paintRect(g);
+        }
     }
 }
